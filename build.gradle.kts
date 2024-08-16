e3266c608aa8882f7f3c598a71ece32be0b90f1e
mkalkowski <michal.kalkowski@sumup.com>
2024-08-16 15:49:04 +0200

    remove not used dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.spotless) apply true
-    alias(libs.plugins.spring.boot) apply false
-    alias(libs.plugins.spring.dependency.management) apply false
 }
 
 allprojects {
     repositories {
         mavenLocal()
         mavenCentral()
-        maven { url = uri("https://packages.confluent.io/maven/") }
         maven { url = uri("https://jitpack.io") }
     }
 }
@@ -32,11 +28,6 @@ subprojects {
     }
 }
 
-ext {
-    set("snakeyaml.version", "2.0") // https://avd.aquasec.com/nvd/2022/cve-2022-1471/
-    set("jackson-bom.version", libs.versions.jackson.get())
-}
-
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
     kotlin {
         target(
@@ -44,7 +35,6 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
                 include("**/*.kt")
                 exclude(
                     "**/.gradle/**",
-                    "**/openapi/**",
                     "**/build/generated/**",
                     "**/buildSrc/build/**",
                 )
@@ -58,30 +48,15 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         target(
             fileTree(".") {
                 include("**/*.gradle.kts")
-                exclude("**/.gradle/**", "**/openapi/**", "**/build/generated/**", "**/buildSrc/build/**")
+                exclude("**/.gradle/**", "**/build/generated/**", "**/buildSrc/build/**")
             },
         )
         trimTrailingWhitespace()
         ktlint()
             .setEditorConfigPath("$rootDir/.editorconfig")
     }
-    sql {
-        target("**/src/main/resources/db/migration/*.sql")
-        dbeaver()
-    }
-    json {
-        target("**/src/test/resources/**/*.json")
-        jackson()
-    }
-    yaml {
-        target("**/src/**/*.yaml")
-        jackson()
-    }
 }
 
 tasks {
     getByName("spotlessKotlinGradle").dependsOn("spotlessKotlin")
-    getByName("spotlessSql").dependsOn("spotlessKotlinGradle")
-    getByName("spotlessJson").dependsOn("spotlessKotlinGradle")
-    getByName("spotlessYaml").dependsOn("spotlessKotlinGradle")
 }
